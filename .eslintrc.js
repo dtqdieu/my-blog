37be6f34f2c30e83bfaac341dd77950df8b80ef5
Đỗ Trung Quang Diệu <dotrungquangdieu@gmail.com>
2018-02-03 23:59:44 +0700

    Update Rule ESlint
    Update Rule ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,5 @@ module.exports = {
             }
         }
     },
-    "rules": {
-        "jsx-a11y/anchor-is-valid": [ "error", {
-            "components": [ "Link" ],
-            "specialLink": [ "hrefLeft", "hrefRight" ],
-            "aspects": [ "noHref", "invalidHref", "preferButton" ]
-          }],
-    },
+    
 };
